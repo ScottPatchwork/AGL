25b26c3842da1c58fdd4ee642ea7d0ff82dc6af5
ScottPatchwork <156607424+ScottPatchwork@users.noreply.github.com>
2024-03-19 11:58:00 +0000

    Add files via upload

---
+++ ANOTHER GIANT LEAP/Player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+var speed = 100
+var speed_default = speed
+var sprint = 150
+var dodge = 100
+func _physics_process (delta):
+	player_movement (delta)
+
+func player_movement (delta):
+	if Input.is_action_pressed("Shift"):
+		speed = sprint
+	else:
+		speed = speed_default
+	
+	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if Input.is_action_just_pressed("ctrl"):
+		velocity = velocity * 3
+	velocity = direction * speed
+	move_and_slide()
+	
+	
+
